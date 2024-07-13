c2f49b4d6967d388a86b6199218828c2f4c40825
thiagobfernandes <thiago5437br@gmail.com>
2024-07-13 00:19:46 -0300

    Desenvolvedor

---
+++ paginas/desenvolvedor.php
@@ -0,0 +1,41 @@
+<section class="w-100 h-100">
+<div class="imagem w-100  " style="height: 600px;">
+   <img src="./imagens/euu.jpeg" alt=""style="height: 600px;" class="w-100" >
+</div>
+<div class="text-light text-center py-2 w-100 d-flex flex-column justify-content-center align-items-center">
+<h1 class="my-2">Quem sou eu?</h1>
+<div class="w-100 d-md-flex my-2  align-items-center justify-content-center bordas rank">
+<div class="w-100 " >
+        <img src="./imagens/thiagoborges.jpeg" alt="" class="w-100 p-2 imagenss "> </div>
+        <div class="text-center text-light w-100 p-2 m-2">
+        <h4 class="py-2">Quem é Thiago Borges Fernandes? </h4>
+    <p class="fs-5">
+        Sou Thiago Borges Fernandes, apaixonado por tecnologia e desenvolvimento
+        web. Tenho conhecimento em PHP, JavaScript, Bootstrap, SASS, HTML e
+        CSS. Atualmente, estou no segundo período de Tecnologia em Análise e
+        Desenvolvimento de Sistemas, onde me dedico a criar soluções inovadoras e
+        eficientes
+</p>
+<a href="curriculo.pdf" target="_blank">
+    <button class="buttonk">
+        <span class="button_lg">
+            <span class="button_sl"></span>
+            <span class="button_text">Meu Curriculo</span>
+        </span>
+    </button>
+</a>
+<div class="redes text-center ">
+<a href="https://github.com/thiagobfernandes/" target="_blank" class="text-light fs-3"><i class="fa-brands fa-github mx-2"></i></a>
+    <a href="https://www.linkedin.com/in/thiago-borges-fernandes-7936ab294/"class="text-light fs-3 mx-2" target="_blank" ><i class="fa-brands fa-linkedin"></i></a>
+    <a href="https://www.instagram.com/thiago_borges135/" class="text-light fs-3 mx-2" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+</div>
+
+    </div>
+    </div>
+    
+</div>
+
+
+
+
+</section>

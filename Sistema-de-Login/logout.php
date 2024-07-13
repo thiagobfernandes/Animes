decfe662d9ebc8d49e6112833114e70459155f86
Thiago Borges Fernandes <141961987+thiagobfernandes@users.noreply.github.com>
2024-07-13 00:25:15 -0300

    Update logout.php
    Ajustando logout

--- Sistema-de-Login/logout.php
+++ Sistema-de-Login/logout.php
@@ -1,24 +1,19 @@
 <?php
 
-// Verifica se os cookies 'nome_usuario' e 'senha_usuario' estão definidos
+
 if(isset($_COOKIE['nome_usuario']) && isset($_COOKIE['senha_usuario'])) {
-    // Obtém os valores dos cookies
+   
     $nome_usuario = $_COOKIE['nome_usuario'];
     $senha_usuario = $_COOKIE['senha_usuario'];
-
-    // Define o tempo de expiração para 7 dias em segundos
+  
     $expiracao = 7 * 24 * 60 * 60; // 7 dias em segundos
-
-    // Desconfigura os cookies 'nome_usuario' e 'senha_usuario'
     setcookie('nome_usuario', '', time() - $expiracao, '/');
     setcookie('senha_usuario', '', time() - $expiracao, '/');
-
-    // Mensagem de confirmação ou redirecionamento após o logout
+   
     echo "Logout realizado com sucesso!";
-    // Redireciona o usuário para a página de login, por exemplo:
-    // header('Location: paginalogin.php');
+    header('Location: paginalogin.php');
 } else {
-    // Caso os cookies não estejam definidos, pode ser que o usuário já tenha feito logout anteriormente
+    
     echo "Não há cookies de login definidos. O usuário já está deslogado.";
 }
 ?>

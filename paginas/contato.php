3d5328ccd19979fb24e345f726a4d01f4f009a35
thiagobfernandes <thiago5437br@gmail.com>
2024-07-13 00:18:52 -0300

    contatos

---
+++ paginas/contato.php
@@ -0,0 +1,73 @@
+<section class="text-center text-light my-4">
+  <h1 class="py-4"> Contato</h1>
+<div class="envolver w-100 d-flex flex-column align-items-center justify-content-center">
+<div class="form-card1 w-50 my-2">
+  <div class="form-card2">
+    <form class="form"  action="https://formsubmit.co/darkindayaox@gmail.com" method="POST">
+      <p class="form-heading">Enviar email</p>
+
+      <div class="form-field">
+        <input required="" placeholder="Name" class="input-field" type="text" name="name" />
+      </div>
+
+      <div class="form-field">
+        <input
+          required=""
+          name="email"
+          placeholder="Email"
+          class="input-field"
+          type="email"
+        />
+      </div>
+
+      <div class="form-field">
+        <input
+          required=""
+          placeholder="Subject"
+          class="input-field"
+         type="text" 
+         name="_subject" >
+        />
+      </div>
+      
+
+      <div class="form-field">
+        <textarea
+          required=""
+          placeholder="Message"
+          cols="30"
+          rows="3"
+          class="input-field"
+          name="message"
+        ></textarea>
+      </div>
+      <input type="hidden" name="_captcha" value="false">
+      <input type="hidden" name="_next" value="http://localhost/animes/Animes/contato">
+
+      <button class="sendMessage-btn">Enviar Mensagem</button>
+    </form>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+</section>

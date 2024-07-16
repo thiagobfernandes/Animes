2179dac2036c89e1dbe6663917b66636ac570233
thiagobfernandes <thiago5437br@gmail.com>
2024-07-15 22:01:23 -0300

    Pagina de Login

--- paginas/paginalogin.php
+++ paginas/paginalogin.php
@@ -1,6 +1,6 @@
 <section class="w-100 h-100 d-flex align-items-center justify-content-center  login ">
 
-    <div class="wrapper w-50 ">
+    <div class="wrapper w-50 my-4 my-md-0 ">
             <div class="card-switch ">
                 
                   
@@ -14,7 +14,14 @@
                                 <input type="text" placeholder="username" name="username" class="flip-card__input" autocomplete="off">
                                 <input type="password" placeholder="senha" name="senha" class="flip-card__input" autocomplete="off">
                                 <button class="flip-card__btn">Let`s go!</button>
-                                <p></p>
+                                <?php if(isset($_COOKIE['usuarioincorreto']) && isset($_COOKIE['senhaincorreta'])){ ?>
+                                
+                                 <p class="text-center text-light py-1">Usuario ou senha Incorreto</p>
+                              
+                                <?php
+                                }
+                                ?>
+                                
                              </form>
                           </div>
                         
@@ -30,7 +37,7 @@
 
 
 </div>
-<div class="imagem w-75 ">
+<div class="imagem w-75 d-none d-md-block ">
 <img src="./imagens/gojologin.jpg" alt="" class="w-100">
 </div>
 

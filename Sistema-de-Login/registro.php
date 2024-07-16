965e12085a8738df11db5f435947697e14bb012f
thiagobfernandes <thiago5437br@gmail.com>
2024-07-15 22:04:10 -0300

    Pagina  para se registrar no banco de dados

--- Sistema-de-Login/registro.php
+++ Sistema-de-Login/registro.php
@@ -6,27 +6,29 @@ $email = $_POST["email"];
 
 include "./conectar.php";
 
-// Prevenção contra SQL Injection
+
 $username_escaped = mysqli_real_escape_string($conexao, $username);
 $email_escaped = mysqli_real_escape_string($conexao, $email);
 
-// Consulta SQL para verificar se o username ou email já existe
+
 $sql = "SELECT COUNT(*) as count FROM users WHERE username='$username_escaped' OR email='$email_escaped'";
 $resultado = mysqli_query($conexao, $sql);
 $row = mysqli_fetch_assoc($resultado);
 
-// Verifica se o usuário ou email já existe
+
 if ($row['count'] > 0) {
-    echo "<script type='text/javascript'>window.location.href = '../registroerro';</script>";
+    setcookie('Registro', 'true', time()+20,'/');
+    echo "<script type='text/javascript'>window.location.href = '../registro';</script>";
     
 } else {
-    // Se não existir, insere o novo usuário
+   
     $senha_escaped = mysqli_real_escape_string($conexao, $senha);
     
     $insert_sql = "INSERT INTO users (username, senha, email) VALUES ('$username_escaped', '$senha_escaped', '$email_escaped')";
     if (mysqli_query($conexao, $insert_sql)) {
         setcookie("nome_usuario", $username, time() + (86400 * 30), "/");
         setcookie("senha_usuario", $senha, time() + (86400 * 30), "/");
+        setcookie('Registro', 'true', time()-20,'/');
 
         // Redireciona para a página de login
         echo "<script type='text/javascript'>window.location.href = '../paginalogin';</script>";
